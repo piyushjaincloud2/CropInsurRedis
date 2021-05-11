d48c05879745c192d98a2e054a8109c9617bce39
LL\chikhann <chikhann@publicisgroupe.net>
2021-05-12 04:58:52 +0530

    added image parameter for client app

--- Redis_Airsim/airsim/gearclient.py
+++ Redis_Airsim/airsim/gearclient.py
@@ -2,18 +2,18 @@ from gearsclient import GearsRemoteBuilder as GearsBuilder
 from gearsclient import execute
 import redis
 import cv2
-import base64
 
 conn = redis.Redis(host='localhost', port=6379)
 
-with open("1.png", "rb") as image_file:
-    encodedimage = base64.b64encode(image_file.read()).decode('utf8')
+MAX_IMAGES = 50
 
+imagename = "2.jpg"
 img0 = cv2.imread("2.jpg")
 _, data = cv2.imencode('.jpg', img0)
 storedimg = data.tobytes()
+msg = {
+    'imagename': imagename,
+    'image': storedimg
+}
 
-print(storedimg)
-
-MAX_IMAGES = 50
-conn.execute_command('xadd', 'airsimrunner',  'MAXLEN', '~', str(MAX_IMAGES), '*', 'image', storedimg)
+conn.execute_command('xadd', 'airsimrunner',  'MAXLEN', '~', str(MAX_IMAGES), '*','imagename', msg['imagename'], 'image', msg['image'])
--- Redis_Airsim/app/gearconsumer.py
+++ Redis_Airsim/app/gearconsumer.py
@@ -12,10 +12,9 @@ from azure.storage.blob import BlobClient
 MAX_IMAGES = 50
 Labels = ["CultivatedLand","InFertileLand","Others"]
 Connection_String = 'DefaultEndpointsProtocol=https;AccountName=droneredissg;AccountKey=hJx8SCf/fYN0zpOJvoAdfLv+JHwDDNO0ZScseOZI4xATYOuU4mI2I4LPbCy/qerO4jrw2zf1AgIC1diDQuayWw==;EndpointSuffix=core.windows.net'
-blob = BlobClient.from_connection_string(conn_str=Connection_String, container_name="droneimages", blob_name="7.jpg")
-blob1 = BlobClient.from_connection_string(conn_str=Connection_String, container_name="droneimages", blob_name="8.jpg")
 
-def add_boxes_to_images(img, predictions,classes):
+
+def add_boxes_to_images(img, predictions,classes,blob):
     try:
         for idx,pred in enumerate(predictions):
             x = int(pred[0] * 600)
@@ -30,11 +29,11 @@ def add_boxes_to_images(img, predictions,classes):
 
             ImageDraw.Draw(img).rectangle(shape, outline ="red") 
             ImageDraw.Draw(img).text((x, y), text, fill ="red", align ="left",font=font)
-        saveResizeImageToAzure(img)    
+        saveImageToAzure(img,blob)    
     except:
         xlog('add_boxes_to_images: error:', sys.exc_info()[0])
 
-def saveImageToAzure(img):
+def saveImageToAzure(img,blob):
     try:
         img_byte_arr = io.BytesIO()
         img.save(img_byte_arr, format='JPEG')
@@ -43,24 +42,14 @@ def saveImageToAzure(img):
     except:
         xlog('saveImageToAzure: error:', sys.exc_info()[0])
 
-def saveResizeImageToAzure(img):
-    try:
-        img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format='JPEG')
-        img_byte_arr = img_byte_arr.getvalue()
-        blob1.upload_blob(img_byte_arr)
-    except:
-        xlog('saveResizeImageToAzure: error:', sys.exc_info()[0])
 
 def predictImage(x):
         try:
             image_data = io.BytesIO(x['value']['image'])
             image = Image.open(image_data)
-            saveImageToAzure(image)
             numpy_img = np.array(image)
             resize_img = cv2.resize(numpy_img, (320, 320), interpolation=cv2.INTER_LINEAR)
             inputs = np.array(resize_img, dtype=np.float32)[np.newaxis, :, :, :]
-
             img_ba = bytearray(inputs.tobytes())
             v1 = redisAI.createTensorFromBlob('FLOAT', [1, 320, 320, 3], img_ba)
 
@@ -81,20 +70,20 @@ def predictImage(x):
                 if(prediction < 0.5):
                     deleteLowProbResult.append(idx)
 
-
             array_2d_rowcount = int(len(res1)/4)
             arr_2d = np.reshape(res1, (array_2d_rowcount, 4))
 
             detectedboxes = np.delete(arr_2d, deleteLowProbResult, axis=0)
             detectedclasses = np.delete(res3, deleteLowProbResult)
-
-            add_boxes_to_images(image,detectedboxes,detectedclasses)
+            
+            imagename = x['value']['imagename']
+            blob = BlobClient.from_connection_string(conn_str=Connection_String, container_name="droneimages", blob_name=imagename)
+            add_boxes_to_images(image,detectedboxes,detectedclasses,blob)
             return res1,res2,res3
         except:
             xlog('Predict_image: error:', sys.exc_info()[0])
 
 def addToStream(x):
-    # save animal name into a new stream
     try:
         redisgears.executeCommand('xadd', 'result1', '*', 'text', x[0])
         redisgears.executeCommand('xadd', 'result2', '*', 'text', x[1])
